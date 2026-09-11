f09b7f3b35f4525a276da87bbe15afeea0edaafd
José Hermes <jose.hermes.dev@gmail.com>
2026-09-11 16:54:35 -0300

    feat: adiciona tipos MovieDetails e CastMember

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -9,3 +9,24 @@ export interface Movie {
   poster_path: string | null;
   genre_ids: number[];
 };
+
+export interface CastMember {
+  id: number;
+  name: string;
+  character: string;
+  profile_path: string | null;
+};
+
+export interface MovieDetails {
+  id: number;
+  title: string;
+  poster_path: string | null;
+  overview: string;
+  vote_average: number;
+  release_date: string;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+}
+
